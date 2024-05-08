4491538f36871c4db72473ecf0162f8dc0ad32e0
adevrent <a.devrent@hotmail.com>
2024-05-08 16:29:06 +0300

    yahoo first implementation complete

--- yahoo.py
+++ yahoo.py
@@ -5,28 +5,29 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 # Set the default end date to today's date as default
-end_date = date.today()
+end = date.today()
 
 # Calculate the default start date, which is one year before the end date as default
-start_date = end_date - relativedelta(years=1)
+start = end - relativedelta(years=1)
 
-def yf_xw(ticker, start_date=None, end_date=None, isFund=False, output_path=None):
+def yf_xw(ticker, start_date=None, end_date=None, output_path=None):
     # Create a new Excel workbook
     wb = xw.Book()
     
     if start_date is None:
-        start_date = pd.to_datetime(start_date, format="%d.%m.%Y")
+        start_date = pd.to_datetime(start, format="%d.%m.%Y")
     
     if end_date is None:
-        end_date = pd.to_datetime(end_date, format="%d.%m.%Y")
+        end_date = pd.to_datetime(end, format="%d.%m.%Y")
     
-    if isFund:
+    data = yf.download(ticker, start_date, end_date)
+    # if data empty, try with "^" symbol as ticket might be a fund
+    if len(data) == 0:
         data = yf.download(f"^{ticker}", start_date, end_date)
-    else:
-        data = yf.download(ticker, start_date, end_date)
-    
+
     # Write dataframe to Excel
     wb.sheets[0].range("A1").value = data
     
 # Run code
-yf_xw("TSLA")
+ticker = "SPY"
+yf_xw(ticker)
