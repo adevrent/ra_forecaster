4d646919645b81c85fd55f950a17e72f5c3935dd
adevrent <a.devrent@hotmail.com>
2024-07-01 14:30:39 +0300

    output formats updated

--- halkarz.py
+++ halkarz.py
@@ -2,6 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def european_to_float(value):
+    """
+    Convert a European formatted string to a float.
+    E.g., "30.416.600" -> 30416600.00
+        "30.416.600,99" -> 30416600.99
+    """
+    if isinstance(value, str):
+        # print("value =", value)
+        value = value.split(" ")[0]  # splits the unit name
+        # print("value after split =", value)
+        value = value.replace('.', '').replace(',', '.')
+        # print("value after replace =", value)
+    try:
+        return float(value)
+    except:
+        return None
+    
+def Pay_to_int(value):
+    print("value =", value)
+    value = value.split(" ")[0]
+    print("value after split =", value)
+    value = value.replace(",", "")
+    print("value after replace =", value)
+    
+    return int(value)
+
 def get_halkarz_info():
     # Define headers with a User-Agent
     headers = {
@@ -64,11 +90,26 @@ def get_halkarz_info():
 
                         # Extract Bist Kodu for index
                         bist_kodu = company_info.get("Bist Kodu", "Unknown")
+                        company_title_tag = entry.find('h3', class_='il-halka-arz-sirket')
+                        company_title = company_title_tag.text.strip()
+                        company_info["İhraççı"] = company_title
+                        print(f"İhraççı: {company_title}")
+                        
 
                         # Create a Pandas Series and append it to the dataframe
                         series = pd.Series(company_info, name=bist_kodu)
                         df = pd.concat([df, pd.DataFrame(series).T])
 
+    # Re-formatting
+    
+    # Convert "Halka Arz Fiyatı/Aralığı" to float using the european_to_float function
+    if "Halka Arz Fiyatı/Aralığı" in df.columns:
+        df["Halka Arz Fiyatı/Aralığı"] = df["Halka Arz Fiyatı/Aralığı"].apply(european_to_float)
+        
+    # Convert "Pay" to float
+    if "Pay" in df.columns:
+        df["Pay"] = df["Pay"].apply(Pay_to_int)
+    
     return df
 
 # Test
